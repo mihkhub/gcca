799f25b9c2f4c4aa4a25aa27de3321f1823c28aa
mihkhub <changfeng.or.chen79@gmail.com>
2017-07-09 22:55:23 +0800

    Specifies a minimu alignment for the variable or structure field, measured in bytes.

--- 4.8.5/ch06/6.36/aligned/aligned.c
+++ 4.8.5/ch06/6.36/aligned/aligned.c
@@ -4,7 +4,7 @@
 #define B_MASK_16 (uint64_t)0xf
 
 int x __attribute__(( aligned(16) )) = 0;
-// causes the compiler to allocate the global variable x on a 16 byte boundary.
+// causes the compiler to allocate the global variable x on a 16-byte boundary.
 struct foo
 {
 	int x[2] __attribute__ (( aligned(32) ));
@@ -15,14 +15,14 @@ struct foo s;
 int main(int argc, char **argv)
 {
 	uint64_t ptr;
-	printf("&x:%p\n", &x);
+	printf("glocal variable on a 16-byte boundary &x:%p\n", &x);
 	ptr = (uint64_t)&x;	
 	ptr = ptr & ~B_MASK_16;
 	
 	printf("ptr:%p\n", (void*)ptr);
 
 	printf("foo:%p\n",&s);	
-	printf("foo.x0:%p\n",&s.x);	
+	printf(" Specifies the alignment of structure field foo.x0:%p\n",&s.x);	
 	printf("foo.x1:%p\n",&s.x[1]);	
 	printf("__BIGGEST_ALIGNMENT__: %u\n", __BIGGEST_ALIGNMENT__);
 	return 0;

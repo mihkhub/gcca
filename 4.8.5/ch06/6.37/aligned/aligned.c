323ce94d3ab184377197247d829acd1e688fdac3
mihkhub <changfeng.or.chen79@gmail.com>
2017-07-09 22:31:29 +0800

    Specifies that each member of the structure or union is placed to minimize the memory  required

--- 4.8.5/ch06/6.37/aligned/aligned.c
+++ 4.8.5/ch06/6.37/aligned/aligned.c
@@ -3,6 +3,7 @@
  * specified type. For example, the declarations: */
 struct S { short f[3]; } __attribute__ (( aligned(8 ) ));
 typedef int more_aligned_int __attribute__ (( aligned(8) )) ;
+enum N { A,B,NUM} __attribute__ (( aligned(8) )) ;
 int main(int argc, char **argv)
 {
 	struct S s;
@@ -14,5 +15,8 @@ int main(int argc, char **argv)
 	/*force the compiler to ensure (as far as it can) that each variable whoes type
  * is struct S or more_aligned_int is allocated and aligned at lest on a 8-byte boundary.	
  * 	*/
+	enum N n;	
+	printf("&n=%p\n", &n);
+	printf("sizeof(n)=%zu of enum N\n", sizeof(n) );
 	return 0;
 }
